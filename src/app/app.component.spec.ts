242ed89d9e45fa7bf9e9ed64d0cf78e80dc338d2
EL AINE El Mehdi <ainemehdi6@gmail.com>
2023-05-03 20:17:49 +0200

    style and firebase hosting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'iut-app'`, () => {
+  it(`should have as title 'seances-musculation-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('iut-app');
+    expect(app.title).toEqual('seances-musculation-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('iut-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('seances-musculation-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iut-app';
+  title = 'seances-musculation-app';
 }
